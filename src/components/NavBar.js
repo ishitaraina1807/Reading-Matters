7a434a18b97b8f42593f6d07b50082b20a89b551
Mermaido <ishitaraina18@gmail.com>
2023-08-13 19:20:59 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-      <nav className='navbar wrapper'>
-        <Link to="/"><h1>Reading Matters</h1></Link>
-        <div>
-        <a href=''><span>About</span></a>
-        <a href='https://github.com/ishitaraina1807' target="_blank"><span>Contact</span></a>
+      <nav className={`navbar wrapper ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/">
+          <h1>Reading Matters</h1>
+        </Link>
+        <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+          <a href="">
+            <span>About</span>
+          </a>
+          <a href="https://github.com/ishitaraina1807" target="_blank">
+            <span>Contact</span>
+          </a>
+        </div>
+        <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
       </nav>
     </div>
-  )
+  );
 }
