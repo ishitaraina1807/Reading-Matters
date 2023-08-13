0e10249672c917bcb6923e5b518ca28ef3f5ed10
Mermaido <ishitaraina18@gmail.com>
2023-08-13 19:19:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SearchPage from "./components/SearchPage";
 import SearchResultPage from "./components/SearchResultPage";
 import SignUp from "./components/Auth/SignUp";
 import { AuthProvider } from "./components/AuthProvider"; 
+import BookDescription from "./components/BookDescription";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/search-result" element={<SearchResultPage />} />
+          <Route path="/book-description" element={<BookDescription />} />
         </Routes>
         <Footer />
       </AuthProvider>
