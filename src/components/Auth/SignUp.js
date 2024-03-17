36d059f1639626456cd374e704aa23d37ada8743
07Kaustubh <101243527+07Kaustubh@users.noreply.github.com>
2024-03-17 09:04:51 +0000

    improved naming and error handling in signup

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -8,7 +8,7 @@ const SignUp = () => {
   const navigate = useNavigate();
 
   const [values, setValues] = useState({
-    name: "",
+    username: "",
     email: "",
     password: "",
   });
@@ -18,7 +18,7 @@ const SignUp = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    if (values.name === "" || values.email === "" || values.password === "") {
+    if (values.username === "" || values.email === "" || values.password === "") {
       setErrorMsg("All fields are required.");
       return;
     }
@@ -33,78 +33,86 @@ const SignUp = () => {
       const user = userCredential.user;
 
       await updateProfile(user, {
-        displayName: values.name,
-      });
-      console.log("New user created with credentials: ", user);
-      navigate("/search", { state: { name: values.name } });
-
-      await setDoc(doc(db, "users", user.uid), {
-        name: values.name,
-        favoriteBooks: [], 
-        readingList: [], 
+        displayName: values.username,
       });
 
-    } catch (e) {
-      console.error("Error signing up: ", e);
-      setErrorMsg("Error signing up. Please try again.");
+      const userData = {
+        name: values.username,
+        favoriteBooks: [],
+        readingList: [],
+      };
+
+      await setDoc(doc(db, "users", user.uid), userData);
+
+      navigate("/search", { state: { name: values.username } });
+    } catch (error) {
+      console.error("Error signing up: ", error);
+      const errorCode = error.code;
+      let errorMessage = "Error signing up. Please try again.";
+
+      if (errorCode === "auth/email-already-in-use") {
+        errorMessage = "Email address is already in use. Please use a different one.";
+      }
+
+      setErrorMsg(errorMessage);
     }
   };
 
   return (
     <div className="p-6 text-white flex justify-center items-center min-h-screen">
-    <div className="flex justify-center items-center h-full">
-      <div className="w-80 bg-darkblue p-6 rounded shadow">
-        <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
-        <form onSubmit={handleSignup}>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-white font-bold mb-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
-              value={values.name}
-              onChange={(event) =>
-                setValues((prev) => ({ ...prev, name: event.target.value }))
-              }
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-white font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
-              value={values.email}
-              onChange={(event) =>
-                setValues((prev) => ({ ...prev, email: event.target.value }))
-              }
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-white font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
-              value={values.password}
-              onChange={(event) =>
-                setValues((prev) => ({
-                  ...prev,
-                  password: event.target.value,
-                }))
-              }
-              required
-            />
-          </div>
-          <div className="flex justify-center items-center text-sm my-2">
+      <div className="flex justify-center items-center h-full">
+        <div className="w-80 bg-darkblue p-6 rounded shadow">
+          <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+          <form onSubmit={handleSignup}>
+            <div className="mb-4">
+              <label htmlFor="username" className="block text-white font-bold mb-2">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+                value={values.username}
+                onChange={(event) =>
+                  setValues((prev) => ({ ...prev, username: event.target.value }))
+                }
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-white font-bold mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+                value={values.email}
+                onChange={(event) =>
+                  setValues((prev) => ({ ...prev, email: event.target.value }))
+                }
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-white font-bold mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+                value={values.password}
+                onChange={(event) =>
+                  setValues((prev) => ({
+                    ...prev,
+                    password: event.target.value,
+                  }))
+                }
+                required
+              />
+            </div>
+            <div className="flex justify-center items-center text-sm my-2">
             <span className="text-gray-800">Already have an account?</span>
             &nbsp;&nbsp;
             <span
@@ -114,18 +122,18 @@ const SignUp = () => {
               Login
             </span>
           </div>
-          {errorMsg && <p className="text-red-300 mt-2 mb-2 text-sm flex justify-center">{errorMsg}</p>}
+            {errorMsg && <p className="text-red-300 mt-2 mb-2 text-sm flex justify-center">{errorMsg}</p>}
 
-          <button
-            type="submit"
-            className="w-full bg-white text-darkblue rounded py-2 hover:text-white hover:bg-lightblue"
-          >
-            Sign Up
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="w-full bg-white text-darkblue rounded py-2 hover:text-white hover:bg-lightblue"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
