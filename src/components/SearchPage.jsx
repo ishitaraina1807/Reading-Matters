ff542cc3283b9c082ae8a14bab9f737453024056
Gourav-21 <rahul.dora21@gmail.com>
2024-03-19 05:35:29 +0530

    components converted to tsx

---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import searchIconSvg from '../images/searchiconsvg.png';
+import search from '../images/search.svg';
+import { Link } from "react-router-dom";
+
+export default function SearchPage() {
+  const location = useLocation();
+  const name = location.state?.name || "User";
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    if (searchQuery.trim() !== "") {
+      navigate(`/search-result?query=${encodeURIComponent(searchQuery)}`);
+    }
+  };
+
+  return (
+    <div className='searchpage'>
+      <div className="search-section">
+        <div>
+          <div className="mt-0 border-t border-gray-100">
+            <dl className="divide-y divide-gray-100">
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt className="text-2xl leading-6 text-customblack font-thin">Heyyy {name}!</dt>
+                <dd className="mt-2 text-sm sm:col-span-2 sm:mt-0">
+                  <ul role="list" className="divide-y divide-darkblue rounded-md border border-darkblue">
+                    <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                      <div className="flex w-0 flex-1 items-center">
+                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                          <span className="truncate font-medium">Your Favorite Books - upcoming feature</span>
+                        </div>
+                      </div>
+                      <div className="ml-4 flex-shrink-0">
+                        <Link to='/favorite-books' className="font-medium text-darkblue hover:text-lightblue">
+                          View
+                        </Link>
+                      </div>
+                    </li>
+                    <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                      <div className="flex w-0 flex-1 items-center">
+                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                          <span className="truncate font-medium">Your Reading List - upcoming feature</span>
+                        </div>
+                      </div>
+                      <div className="ml-4 flex-shrink-0">
+                        <Link to='/reading-list' className="font-medium text-darkblue hover:text-lightblue">
+                          View
+                        </Link>
+                      </div>
+                    </li>
+                  </ul>
+                </dd>
+              </div>
+            </dl>
+          </div>
+
+        </div>
+        <p className='text-customblack font-light'>You don't need to know the name of the book, if you know the name of the author or publisher, just type, search and find</p>
+      </div>
+      <div className="search">
+        <input
+          id="search"
+          name="search"
+          type="text"
+          value={searchQuery}
+          onChange={event => setSearchQuery(event.target.value)}
+          className="min-w-0 flex-auto rounded-md border-0 bg-white mr-3 px-8 py-2 text-customblack shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-darkblue sm:text-lg sm:leading-6"
+          placeholder="Type author, book name..."
+          aria-label="Search"
+        />
+        <button
+          type="submit"
+          onClick={handleSearch}
+          className={`rounded-md bg-white px-3.5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-lightblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 ${searchQuery.trim() === "" ? "pointer-events-none opacity-50 cursor-not-allowed" : ""}`}
+          aria-label="Submit Search"
+          disabled={searchQuery.trim() === ""}
+        >
+          <img src={searchIconSvg} alt="search" />
+        </button>
+      </div>
+      <div className="flex justify-center mt-20">
+        <img src={search} alt="books" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SearchResultPage.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const SearchResultPage = () => {
+  const location = useLocation();
+  const searchQuery = new URLSearchParams(location.search).get('query');
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = 'AIzaSyBE1xFqUa2lA0uBrYDhxShBcxIZksHox4Y'; 
+        const response = await fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchQuery)}&key=${apiKey}`
+        );
+        const data = await response.json();
+        setSearchResults(data.items || []);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please try again later.');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [searchQuery]);
+
+  return (
+    <div>
+      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+        <div className='mb-10 text-customblack font-semibold'>Search Results:</div>
+        {loading && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-6">
+          {searchResults.map((book) => (
+            <Link
+              key={book.id}
+              to={{
+                pathname: '/book-description',
+                search: `?bookInfo=${encodeURIComponent(JSON.stringify(book))}`,
+              }}
+              className="group"
+              aria-label={book.volumeInfo.title}
+            >
+              <div className="bookinfo aspect-h-1 aspect-w-1 w-full h-full overflow-hidden rounded-lg bg-white xl:aspect-h-8 xl:aspect-w-7">
+                {book.volumeInfo.imageLinks && (
+                  <img
+                    className="object-cover"
+                    src={
+                      book.volumeInfo.imageLinks.thumbnail ||
+                      book.volumeInfo.imageLinks.smallThumbnail
+                    }
+                    alt={book.volumeInfo.title}
+                  />
+                )}
+                <div className="book-info-content mx-5 my-2">
+                  <h1 className="font-semibold text-xl text-customblack">
+                    {book.volumeInfo.title}
+                  </h1>
+                  <p className="mt-1 text-sm font-thin text-gray-900">
+                    <b>Author: </b>
+                    {book.volumeInfo.authors
+                      ? book.volumeInfo.authors.join(', ')
+                      : 'Unknown'}
+                  </p>
+                  <p className="mt-1 text-sm font-thin text-gray-900">
+                    <b>Category: </b>
+                    {book.volumeInfo.categories
+                      ? book.volumeInfo.categories.join(', ')
+                      : 'Unknown'}
+                  </p>
+                  <p className="mt-1 text-sm font-thin text-gray-900">
+                    <b>Published Date: </b>
+                    {book.volumeInfo.publishedDate}
+                  </p>
+                  <div className="mt-5">
+                    <Link
+                      to={{
+                        pathname: '/book-description',
+                        search: `?bookInfo=${encodeURIComponent(
+                          JSON.stringify(book)
+                        )}`,
+                      }}
+                      className="rounded-md bg-darkblue px-2 py-1.5 button text-white shadow-sm hover:bg-lightblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      More <span>&#8594;</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResultPage;
