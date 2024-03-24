fae436306b8a7f8d1b32ce714a7bc4a0050b7ae8
Gourav <rahul.dora21@gmail.com>
2024-03-24 21:34:20 +0530

    lint error fix

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
 import searchIconSvg from '../images/searchiconsvg.png';
 import search from '../images/search.svg';
@@ -68,7 +68,7 @@ export default function SearchPage() {
             </div>
           </dl>
         </div>
-        <p className='text-customblack font-light'>You don't need to know the name of the book, if you know the name of the author or publisher, just type, search and find</p>
+        <p className='text-customblack font-light'>You don&apos;t need to know the name of the book, if you know the name of the author or publisher, just type, search and find</p>
       </div>
 
       {/* Advanced Search Options */}
