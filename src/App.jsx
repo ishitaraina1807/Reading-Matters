4f8b90456753064291cfc601b2aed9723b64d66c
Alvin <alvindennis80@gmail.com>
2024-03-31 22:01:41 +0530

    Login,signupusing google,loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
-import Login from "./components/Auth/Login";
+import Login from "./components/Login";
 import SearchPage from "./components/SearchPage";
 import SearchResultPage from "./components/SearchResultPage";
-import SignUp from "./components/Auth/SignUp";
-import { AuthProvider } from "./components/AuthProvider"; 
+import SignUp from "./components/SignUp";
 import BookDescription from "./components/BookDescription";
+import  BookLoader  from "./components/Loader";
 
 
 function App() {
@@ -19,25 +19,37 @@ function App() {
     setIsDarkMode(!isDarkMode);
     document.body.classList.toggle('dark-mode');
   };
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading (true);
+    setTimeout(() => {
+      setLoading(false);
+    },4000)
+
+  }, []);
 
   return (
-    <div className={`app ${isDarkMode ? 'dark-mode' : ''}`}>
-      <AuthProvider> 
-        <NavBar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+        <div>
+        {
+        loading ? 
+        <div style={{minHeight:"100vh", width: "100%", display:"flex",justifyContent:"center",alignItems:"center"}}>
+          <BookLoader  loading={loading} />
+          </div>
+           :
+        <><NavBar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/search-result" element={<SearchResultPage />} />
-          <Route path="/book-description" element={<BookDescription />} />
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/search-result" element={<SearchResultPage />} />
+                <Route path="/book-description" element={<BookDescription />} />
         </Routes>
-        <Footer />
-      </AuthProvider>
-
+              <Footer />
+              </>
+              }
 </div>
 
   );
-}
-
+            }
 export default App;
